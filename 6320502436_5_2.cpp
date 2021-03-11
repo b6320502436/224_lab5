b57f301d80cb0f3e37b4bd8344ab5e85163340d0
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-11 16:28:51 +0800

    Update 6320502436_5_2.cpp

--- 6320502436_5_2.cpp
+++ 6320502436_5_2.cpp
@@ -12,7 +12,7 @@ int main()
 	{
 		c=(a-1)/2;
 	}else c=a/2;
-	for(x=0;x<a;x++)
+	for(x=0;x<c+1;x++)
 	{
 		for(y=x;y<c;y++)
 		{
@@ -37,11 +37,35 @@ int main()
 		{
 			printf("-");
 		}
-//s
-			
-		
-		
-		printf("\n");
+	printf("\n");
+	}
+	
+	for(x=0;x<c+1;x++)
+	{
+		for(y=x;y<c;y++)
+		{
+			printf("-");
+		}
+			if(x<c+1)
+		{
+		printf("*");
+	}
+		if(x!=0&&x<c+1)
+		{
+			for(z=x;z>0;z--)
+			printf("-");
+			for(z=x-1;z>0;z--)
+			printf("-");
+		}
+			if(x<c+1&&x!=0)
+		{
+		printf("*");
+	}
+		for(y=x;y<c;y++)
+		{
+			printf("-");
+		}
+	printf("\n");
 	}
 	
 	
