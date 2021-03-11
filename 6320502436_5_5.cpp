2c78ab9ffd8fd040323d5398931bd310676529dd
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-11 17:53:52 +0800

    Create 6320502436_5_5.cpp

---
+++ 6320502436_5_5.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int A[a][a]={},c,d,e,f,g,h,i,j,k,l,B[100]={};
+	for(c=0;c<a;c++)
+	{
+		for(d=0;d<a;d++)
+		{
+			scanf("%d",&A[c][d]);
+		}
+	}
+	for(e=0;e<a-b;e++)
+	{
+		for(f=0;f<a-b;f++)
+		{
+			for(i=e;i<b;i++)
+			{
+				for(j=f;)
+			}
+			
+			
+			
+			
+		}
+	}
+	
+}
