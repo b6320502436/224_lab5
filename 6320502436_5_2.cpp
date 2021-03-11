7c22d26a865eff775ac6c4c47af8ba4a77bba0d8
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-11 16:16:09 +0800

    Update 6320502436_5_2.cpp

--- 6320502436_5_2.cpp
+++ 6320502436_5_2.cpp
@@ -1,14 +1,31 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c,d,e,f;
-	scanf("%d",a);
+	int a,b,c,d,e,f,x,y,z;
+	scanf("%d",&a);
 	if(a%2==0)
 	{
 		b=1;
 	}
 	else b=0;
-	
+	if(b==1)
+	{
+		c=(a-1)/2;
+	}else c=a/2;
+	for(x=0;x<a;x++)
+	{
+		for(y=x;y<c;y++)
+		{
+			printf("-");
+		}
+		printf("*");
+		if(x!=0)
+		{
+			for(z=x;z>0;z--)
+			printf("-");
+		}
+		printf("\n");
+	}
 	
 	
 	
