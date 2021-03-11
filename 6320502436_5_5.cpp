cf4349af4bcb9bab1d69cc0f48a77f7387baa49b
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-11 18:02:51 +0800

    Update 6320502436_5_5.cpp

--- 6320502436_5_5.cpp
+++ 6320502436_5_5.cpp
@@ -3,7 +3,7 @@ int main()
 {
 	int a,b;
 	scanf("%d %d",&a,&b);
-	int A[a][a]={},c,d,e,f,g,h,i,j,k,l,B[100]={};
+	int A[a][a]={},c,d,e,f,g,h,i,j,k=0,l,B[100]={},x,max=0;
 	for(c=0;c<a;c++)
 	{
 		for(d=0;d<a;d++)
@@ -11,19 +11,30 @@ int main()
 			scanf("%d",&A[c][d]);
 		}
 	}
-	for(e=0;e<a-b;e++)
+	for(e=0;e<a-b+1;e++)
 	{
-		for(f=0;f<a-b;f++)
+		for(f=0;f<a-b+1;f++)
 		{
-			for(i=e;i<b;i++)
+			for(i=e;i<e+b;i++)
 			{
-				for(j=f;)
+				for(j=f;j<f+b;j++)
+				{
+					B[k]=B[k]+A[i][j];
+				}
 			}
-			
+			k++;
 			
 			
 			
 		}
+	}
+	for(x=0;x<10;x++)
+	{
+		if(B[x]<B[x])
+		{
+			
+		}
+		//printf("%d ",B[x]);
 	}
 	
 }
