2311114a01f511a2906ba26ee805559798855291
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-11 18:47:04 +0800

    Update 6320502436_5_1.cpp

--- 6320502436_5_1.cpp
+++ 6320502436_5_1.cpp
@@ -1,33 +1,46 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	int A[10]={},a,b,c,d=0,e,f,g;
-	char B[30]={};
-	scanf("%s")
-	a=stdren(B);
-	for(b=0;b<a;b++)
+	int A[10]={},a,b,c,d=0,e,f,g,x,y;
+	
+	scanf("%d",&x);
+	char B[x*2+1]={};
+	scanf("%s",B);
+	
+	for(b=0;b<30;b++)
 	{
-		if(B[d]=='+')
+		if(B[b]=='+')
 		{
 			A[d-1]=A[d]+A[d-1];
 			d--;
 		}
-		if(B[d]=='-')
+		if(B[b]=='-')
 		{
 			A[d-1]=A[d]-A[d-1];
 			d--;
 		}
-		if(B[d]=='*')
+		if(B[b]=='*')
 		{
 			A[d-1]=A[d]*A[d-1];
 			d--;
 		}
 		if(B[b]>=48&&B[b]<=57)
 		{
-			A[d]=B[b]-48;
+			A[b]=B[b]-48;
+			b++;
+		}
+		if(B[b]>=48&&B[b]<=57&&B[b+1]>=48&&B[b+1]<=57)
+		{
+			A[b]=(B[b]-48)*10+B[b+1];
+			b++;
 			d++;
 		}
+		else;
+		for(y=0;y<10;y++)
+	    printf("%d",A[y]);
 		
 	}
-	
+	for(b=0;b<x;b++)
+	printf("%d",A[b]);
 }
